1f486304d74cc45ee0acd7b4cd84b80777188931
Sarvesh30112002 <97537477+Sarvesh30112002@users.noreply.github.com>
2023-04-10 05:53:37 +0530

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,24 @@
+/**Counter Animation**/
+let nCount = function (selector) {
+    $(selector).each(function () {
+        $(this).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (value) {
+                $(this).text(Math.ceil(value));
+            }
+        });
+    });
+};
+let a = 0;
+$(window).scroll(function () {
+    let oTop = $(".section-2").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+        a++;
+        nCount('.rect>h1');
+    }
+
+});
+
